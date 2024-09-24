0e2fb8534f43350039537096fe47d7eed78d5de7
jeonnamoo <jnamoo2002@gmail.com>
2024-09-24 09:50:29 +0900

    뼈대만들기

---
+++ Character_grass.py
@@ -0,0 +1,23 @@
+from pico2d import *
+
+def run_rectangle():
+    pass
+def run_circle():
+    pass
+
+open_canvas()
+
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+
+while True:
+    run_rectangle()
+    run_circle
+
+
+
+
+    
+    
+close_canvas()
